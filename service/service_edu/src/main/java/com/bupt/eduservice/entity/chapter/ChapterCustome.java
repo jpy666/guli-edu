e7fdfa681c1fbe98dd36feb7dcaff848b7ca9d44
jpy666 <1798776596@qq.com>
2020-08-27 11:14:39 +0800

    'test'

---
+++ service/service_edu/src/main/java/com/bupt/eduservice/entity/chapter/ChapterCustome.java
@@ -0,0 +1,19 @@
+package com.bupt.eduservice.entity.chapter;
+
+import lombok.Data;
+
+/**
+ * <p>Title:ChapterCustome</p>
+ * <p>Description:</P>
+ * <p>Company:hhu.edu.cn</p>
+ *
+ * @Author 北京邮电大学.金培源
+ * @Date 2020/8/27 11:05
+ * Version 1.0
+ */
+@Data
+public class ChapterCustome {
+    //这是一个测试类
+    private String name;
+    private Integer id;
+}
